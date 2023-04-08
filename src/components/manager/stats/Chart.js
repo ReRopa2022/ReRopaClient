19581a87b4590298b48f5c829de83c2ed4a2f5c1
Assaf <asafalt61@gmail.com>
2023-04-08 15:10:02 +0300

    adding donations stats graph

---
+++ src/components/manager/stats/Chart.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const Chart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis dataKey="count" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="date" fill="#8884d8" />
+        <Bar label={true} dataKey="count" fill="#82ca9d" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Chart;
